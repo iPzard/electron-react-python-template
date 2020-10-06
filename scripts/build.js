5945e126be8ee11dacf9b2a43125d201ad1c3e84
unknown <wade.daniel.w@gmail.com>
2020-10-05 21:12:45 -0700

    updating JS Doc documentation

--- scripts/build.js
+++ scripts/build.js
@@ -1,51 +1,50 @@
 const { spawnSync } = require('child_process');
-const [ , , ...sysArgs ] = process.argv;
-const [ type = 'default' ] = sysArgs;
 
 /**
- * @namespace Building
+ * @namespace Builder
  * @description - Builds React & Python builds of project so Electron can be used.
- * @argument type - Technology to build (e.g., python, react, or all).
  */
 
-switch(type) {
-  case 'react':
-    return buildReact();
+class Builder {
 
-  case 'python':
-    return buildPython();
-
-  case 'all':
-  case 'default':
-  default:
-    return buildAll();
-};
-
-function buildAll() {
-  buildPython();
-  buildReact();
-};
-
-function buildPython(){
-  const app = 'app.py';
-  const icon = './public/favicon.ico';
   /**
-   * @namespace PyInstaller
-   * @description - Child process to convert Python app into an executable (.exe).
+   * @description - Creates React and Python production builds.
+   * @memberof Builder
    */
+  buildAll = () => {
+    const { buildPython, buildReact } = this;
+    buildPython();
+    buildReact();
+  }
 
-  const options = [
-    '--noconsole',
-    '--noconfirm',
-    '--distpath ./resources',
-    `--icon ${icon}`
-  ].join(' ');
+  /**
+   * @description - Creates production build of Python back end.
+   * @memberof Builder
+   */
+  buildPython = () => {
+    console.log('Creating Python distribution files...');
+    
+    const app = 'app.py';
+    const icon = './public/favicon.ico';
+  
+    const options = [
+      '--noconsole',
+      '--noconfirm',
+      '--distpath ./resources',
+      `--icon ${icon}`
+    ].join(' ');
+  
+    spawnSync(`pyinstaller ${options} ${app}`, { detached: false, shell: true, stdio: 'inherit' });
+  }
 
-  console.log('Creating Python distribution files...');
-  spawnSync(`pyinstaller ${options} ${app}`, { detached: false, shell: true, stdio: 'inherit' });
-};
+  /**
+   * @description - Creates production build of React front end.
+   * @memberof Builder
+   */
+  buildReact = () => {
+    console.log('Creating React distribution files...');
+    spawnSync(`react-scripts build`, { detached: false, shell: true, stdio: 'inherit' });
+  }
+}
 
-function buildReact() {
-  console.log('Creating React distribution files...');
-  spawnSync(`react-scripts build`, { detached: false, shell: true, stdio: 'inherit' });
-};
+module.exports.Builder = Builder;
--- scripts/package.js
+++ scripts/package.js
@@ -1,108 +1,115 @@
 const { spawnSync } = require('child_process');
-const [ , , ...sysArgs ] = process.argv;
-const [ os ] = sysArgs;
+const { Builder } = require('./build');
+const builder = new Builder();
 
-/**
- * @namespace Packaging
- * @description - Packages app for various operating systems.
- * @argument os - OS you would like to build for (e.g., windows or mac).
- */
-
-switch(os) {
-  case 'windows':
-    return packageWindows();
-
-  case 'mac':
-    return packageMacOS();
-
-  default:
-    return console.error('No operating system selected.')
-}
-
-function packageMacOS(){
-
-  // TODO: Add installer
-  const options = [
-    'app',
-    '--asar',
-    '--extra-resource=./resources/app',
-    '--icon ./public/favicon.ico',
-    '--darwin',
-    '--out',
-    './dist/macOS',
-    '--overwrite'
-  ].join(' ');
-
-  // Run Electron package manager
-  spawnSync(`electron-packager . ${options}`, { detached: false, shell: true, stdio: 'inherit' });
+// Define input and output directories
+const path = (directory) => {
+  return require('path').resolve(__dirname, directory);
 };
 
-function packageWindows(){
+/**
+ * @namespace Packager
+ * @description - Packages app for various operating systems.
+ */
+class Packager {
 
-  console.log('Building windows package...');
+  /**
+   * @description - Creates DMG installer for macOS.
+   * @memberof Packager
+   */
+  packageMacOS = () => {
 
-  // Build Python & React distribution files
-  require('./build');
+    // Build Python & React distribution files
+    builder.buildAll();
 
-  // Options for electron packager
-  const options = [
-    'app',
-    '--asar',
-    '--extra-resource=./resources/app',
-    '--icon ./public/favicon.ico',
-    '--win32',
-    '--out',
-    './dist/windows',
-    '--overwrite'
-  ].join(' ');
+    const options = {
+      build: [
+        'app',
+        '--asar',
+        '--extra-resource=./resources/app',
+        '--icon ./public/favicon.ico',
+        '--darwin',
+        '--out',
+        './dist/mac',
+        '--overwrite'
+      ].join(' '),
 
-  // Run Electron package manager
-  spawnSync(`electron-packager . ${options}`, { detached: false, shell: true, stdio: 'inherit' });
+      package: [
+        path('../dist/mac/app-darwin-x64/app.app'),
+        'Example',
+        `--out=${path('../dist/mac/setup')}`,
+        `--icon=${path('../utilities/dmg/images/icon.icns')}`,
+        // `--background=${path('../utilities/dmg/images/background.png')}`,
+        `--title="Example App"`,
+        `--overwrite`
+      ].join(' '),
 
-  // Import Modules
-  const { MSICreator } = require('electron-wix-msi');
-  const path = (directory) => require('path').resolve(__dirname, directory);
+      spawn: { detached: false, shell: true, stdio: 'inherit' }
+    };
 
-  // Define input and output directory.
-  const appDirectory = path('../dist/windows/app-win32-x64');
-  const outputDirectory = path('../dist/windows/setup');
-  const appIconPath = path('../utilities/msi/images/icon.ico');
-  const appBackgroundPath = path('../utilities/msi/images/background.png');
-  const appBannerPath = path('../utilities/msi/images/banner.png');
+    spawnSync(`electron-packager . ${options.build}`, options.spawn);
+    spawnSync(`electron-installer-dmg ${options.package}`, options.spawn);
+  };
 
 
-  // Instantiate the MSICreator
-  const msiCreator = new MSICreator({
-      appDirectory,
-      appIconPath,
-      outputDirectory,
+  /**
+   * @description - Creates MSI installer for Windows.
+   * @memberof Packager
+   */
+  packageWindows = () => {
+
+    console.log('Building windows package...');
+
+    // Build Python & React distribution files
+    builder.buildAll();
+
+    const options = {
+      app: [
+        'app',
+        '--asar',
+        '--extra-resource=./resources/app',
+        '--icon ./public/favicon.ico',
+        '--win32',
+        '--out',
+        './dist/windows',
+        '--overwrite'
+      ].join(' '),
+
+      spawn: { detached: false, shell: true, stdio: 'inherit' }
+    };
+
+    spawnSync(`electron-packager . ${options.app}`, options.spawn);
+
+    const { MSICreator } = require('electron-wix-msi');
+
+    const msiCreator = new MSICreator({
+      appDirectory: path('../dist/windows/app-win32-x64'),
+      appIconPath: path('../utilities/msi/images/icon.ico'),
+      outputDirectory: path('../dist/windows/setup'),
       description: 'Example app',
       exe: 'app',
       manufacturer: 'Example Manufacturer',
-      name: 'Example name',
+      name: 'Example App',
       ui: {
         chooseDirectory: true,
         images: {
-          background: appBackgroundPath,
-          banner: appBannerPath,
+          background: path('../utilities/msi/images/background.png'),
+          banner: path('../utilities/msi/images/banner.png'),
         }
       },
       version: '1.0.0',
-  });
+    });
+
+    // Customized MSI template
+    msiCreator.wixTemplate = msiCreator.wixTemplate
+      .replace(/ \(Machine - MSI\)/gi, '')
+      .replace(/ \(Machine\)/gi, '');
 
 
-  // Customized MSI template
-  msiCreator.wixTemplate = msiCreator.wixTemplate
-    .replace(/ \(Machine - MSI\)/gi, '')
-    .replace(/ \(Machine\)/gi, '')
-    .replace(/ \(User - MSI\)/gi, '')
-    .replace(/ \(User\)/gi, '');
+    // Create .wxs template and compile MSI
+    msiCreator.create().then(() => msiCreator.compile());
+  };
 
+}
 
-  // Create a .wxs template for MSI
-  msiCreator.create().then(() => {
-
-    // Compile .msi file
-    msiCreator.compile();
-  });
-};
+module.exports.Packager = Packager;
