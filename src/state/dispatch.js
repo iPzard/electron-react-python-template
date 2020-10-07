27939c7f5565681d79418be758efba2926f73077
unknown <wade.daniel.w@gmail.com>
2020-10-06 17:45:39 -0700

    updated redux store to self automated dispatch and reducer combining

--- src/state/dispatch.js
+++ src/state/dispatch.js
@@ -2,10 +2,10 @@ import * as actionModules from 'state/actions';
 
 export const mapStateToProps = state => ({ state });
 
-export const mapDispatchToProps = (dispatch) => {
-  const actions = [ ...Object.values(actionModules) ];
+const actions = [ ...Object.values(actionModules) ];
 
-  return actions.reduce((acc, action) => {
-    return acc = { ...acc, ...action(dispatch) }
-  }, {});
-};
+export const mapDispatchToProps = (dispatch) => (
+  actions.reduce((acc, action) => (
+    acc = { ...acc, ...action(dispatch) }
+  ), {})
+);
